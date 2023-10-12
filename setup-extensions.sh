cf21775cd9d91ca8835ebdc369c7d895a253ac02
Sudheera Sandaruwan Jayasinghe <100751994+sudheera-jayasinghe-ksft@users.noreply.github.com>
2023-10-12 15:00:36 +0530

    Updated setup-extensions script

--- setup-extensions.sh
+++ setup-extensions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml -f extensions/fleet/fleet-compose.yml -f extensions/filebeat/filebeat-compose.yml -f extensions/heartbeat/heartbeat-compose.yml -f extensions/logspout/logspout-compose.yml -f extensions/metricbeat/metricbeat-compose.yml up -d
+docker compose -f docker-compose.yml -f extensions/fleet/fleet-compose.yml -f extensions/fleet/agent-apmserver-compose.yml -f extensions/filebeat/filebeat-compose.yml -f extensions/heartbeat/heartbeat-compose.yml -f extensions/metricbeat/metricbeat-compose.yml up -d
